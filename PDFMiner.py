0025e66c0590e7723c00367985d80edcc586253b
TiTi <40444106+koutiany@users.noreply.github.com>
2023-10-30 01:11:31 -0400

    Create PDFMiner.py

---
+++ PDFMiner.py
@@ -0,0 +1,10 @@
+from pdfminer.high_level import extract_text
+ 
+def extract_text_from_pdf(pdf_path):
+    text = extract_text(pdf_path)
+    return text
+ 
+if __name__ == "__main__":
+ pdf_path = '/home/tippy/Downloads/WritingSample_KH.pdf'  # Replace with actual path
+ extracted_text = extract_text_from_pdf(pdf_path)
+ print(extracted_text)
